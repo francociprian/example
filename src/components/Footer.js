a4035ecc8beb82908545885123eedeb85ad4c203
Franco Ciprian <franco_ciprian@hotmail.com>
2022-09-06 11:00:53 -0300

    updating footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { SiGmail, SiLinkedin, SiGithub } from "react-icons/si";
 
 export default function Footer() {
   return (
-    <div className='text-[#1F2626] dark:text-[#BDEBEA] text-center'>
-      <div>
-        <ul  className='flex justify-center items-center gap-5'>
+    <div className='my-10 text-[#1F2626] dark:text-[#DBFFFF] text-center max-w-sm mx-auto'>
+      <div className='py-10 '>
+        <ul  className='flex justify-between items-center'>
           <li>
             <a 
               href='https://gmail.com/'
               className='flex flex-col items-center justify-center'
               >
-              <SiGmail /> GMAIL
+              <SiGmail className='text-[#5222D0] dark:text-[#00F5A0] text-3xl' /> GMAIL
             </a>
           </li>
           <li>
@@ -19,7 +20,7 @@ export default function Footer() {
               href='https://www.linkedin.com/'
               className='flex flex-col items-center justify-center'
             >
-              <SiLinkedin /> LINKEDIN 
+              <SiLinkedin className='text-[#5222D0] dark:text-[#00F5A0] text-3xl' /> LINKEDIN 
             </a>
           </li> 
           <li>
@@ -27,18 +28,18 @@ export default function Footer() {
               href='https://github.com/'
               className='flex flex-col items-center justify-center'
             >
-              <SiGithub /> GITHUB
+              <SiGithub className='text-[#5222D0] dark:text-[#00F5A0] text-3xl' /> GITHUB
             </a>
           </li>
         </ul>
       </div>
-      <div>
-        <ul>
-          <li><a href='#home'>Projects</a></li>
-          <li><a href='#home'>Contact</a></li>
+      <div className='mb-10'>
+        <ul className='flex justify-evenly'>
+          <li><Link to='/projects'>Projects</Link></li>
+          <li><Link to='/contact'>Contact</Link></li>
         </ul>
       </div>
-      <p>LoremIpsum.com, 2022</p>
+      <p>LoremIpsum.com , 2022</p>
     </div>
   )
 }
