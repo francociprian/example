f86f63ad2180738258fde3637e87a59b27443096
Franco Ciprian <franco_ciprian@hotmail.com>
2022-09-07 11:59:56 -0300

    Updating the responsive of AboutMe

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -3,9 +3,10 @@ import React from 'react'
 export default function AboutMe() {
   return (
     <div className='max-w-6xl mx-auto text-[#1F2626] dark:text-[#BDEBEA]'>
-      <h1 className='tracking-wider text-2xl'>About me</h1>
-      <p>Looking to improve the overall quality of the product through a
-                                    design thinking approach. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos, quae corporis nesciunt aspernatur sapiente magnam. Lorem ipsum dolor sit amet consectetur adipisicing elit. The long barrow was built on land previously inhabited in the Mesolithic period. It consisted of a sub-rectangular earthen tumulus, estimated to have been 15 metres (50 feet) in length, with a chamber built from sarsen megaliths on its eastern end. Both inhumed and cremated human remains were placed within this chamber during the Neolithic period, representing at least nine or ten individuals.</p>
+      <h1 className='tracking-wider text-2xl pl-4 mb-16'>About me</h1>
+      <div className='mx-4'>
+        <p className='px-4 tracking-wider text-lg font-thin '>Looking to improve the overall quality of the product through a design thinking approach. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos, quae corporis nesciunt aspernatur sapiente magnam. Lorem ipsum dolor sit amet consectetur adipisicing elit. The long barrow was built on land previously inhabited in the Mesolithic period. It consisted of a sub-rectangular earthen tumulus, estimated to have been 15 metres (50 feet) in length, with a chamber built from sarsen megaliths on its eastern end. Both inhumed and cremated human remains were placed within this chamber during the Neolithic period, representing at least nine or ten individuals.</p>
+      </div>
     </div>
   )
 }
